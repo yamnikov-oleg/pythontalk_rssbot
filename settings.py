0dc5e2035576de58cc421a19a01b04bffabaeade
Oleg Yamnikov <bgeyts667@gmail.com>
2019-04-10 00:03:52 +0500

    Update headers with self-care advices

--- settings.py
+++ settings.py
@@ -12,12 +12,18 @@ BOT_TOKEN = ''
 BOT_PROXY = ''
 CHAT_ID = ''
 HEADERS = [
-    "So hot right now in {}:",
-    "Why so gloomy? Here're some links from {}:",
-    "Omg, breaking news from {}:",
-    "Cheer up, feed your brain with {}:",
-    "Time to procrastinate with {}:",
-    "Stop whatever you're doing, look at this ({}):",
+    "Take a break, you earned it. New on {}:",
+    "You look stunning today. New on {}:",
+    "Do you like sweets? Treat yourself with a dessert today. New on {}:",
+    "How's the weather today? Good enough for a walk? New on {}:",
+    "Pause, take a deep breath. Let your shoulders drop. New on {}:",
+    "Have you been drinking enough water? New on {}:",
+    "Are sleeping enough? Don't deny yourself a quality rest. New on {}:",
+    "It's never a bad time to listen to your favorite music. New on {}:",
+    "Put a smile on that pretty face! New on {}:",
+    "Don't be too hard on yourself. One small step at a time. New on {}:",
+    "A good meal can make one's day. New on {}:",
+    "There are no bad days without good days coming after. New on {}:",
 ]
 
 UPDATE_EVERY = timedelta(hours=8)
